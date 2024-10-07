fc7ce8db6815cc3556b7fb291a3adeec3fd1b59e
gio10herrera <herrerafontalvo@outlook.com>
2024-10-06 20:55:09 -0500

    componentes instanciados

--- src/ManejadorFrame.java
+++ src/ManejadorFrame.java
@@ -5,10 +5,30 @@ public class ManejadorFrame {
     static JLabel txtLabelX, txtLabelY, txtLabelResult;
     static JTextField txtFieldX, txtFieldY, txtFieldResult;
     static JButton btnSumar, btnRestar, btnMultiplicar, btnDividir, btnRaizX, btnRaizY, btnXaLaY, btnYaLaX, btnXModY;
-    
+
     public static void main(String[] args) {
         inicializarJframeManejador();
         jFrameManejadores.setVisible(true);
+        instanciarComponentes();
+    }
+
+    private static void instanciarComponentes() {
+        txtLabelX = new JLabel("X");
+        txtLabelY = new JLabel("Y");
+        txtLabelResult = new JLabel("Resultado");
+        txtFieldX = new JTextField();
+        txtFieldY = new JTextField();
+        txtFieldResult = new JTextField();
+        txtFieldResult.setEditable(false);
+        btnSumar = new JButton("+");
+        btnRestar = new JButton("-");
+        btnMultiplicar = new JButton("*");
+        btnDividir = new JButton("/");
+        btnRaizX = new JButton("Raiz X");
+        btnRaizY = new JButton("Raiz Y");
+        btnXaLaY = new JButton("X a la Y");
+        btnYaLaX = new JButton("Y a la X");
+        btnXModY = new JButton("X mod Y");
     }
 
     private static void inicializarJframeManejador() {
